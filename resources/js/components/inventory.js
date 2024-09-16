f8eb9a2c3392477af932e3b8e9a351dd7a1777a7
Adebisi Adebambo <debixydev@gmail.com>
2024-09-16 10:48:29 +0100

    Create inventory.js
    Signed-off-by: Adebisi Adebambo <debixydev@gmail.com>

---
+++ resources/js/components/inventory.js
@@ -0,0 +1,9 @@
+// JavaScript for inventory interactions
+document.addEventListener('DOMContentLoaded', () => {
+    const inventoryTable = document.querySelector('.inventory .table');
+
+    if (inventoryTable) {
+        // Example: Add functionality like sorting or filtering
+        console.log('Inventory table loaded.');
+    }
+});
